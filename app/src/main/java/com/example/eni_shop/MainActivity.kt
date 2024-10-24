dd30421700e4ded00d1805024e30cf002b38c52c
thomasdeloneni <149668246+thomasdeloneni@users.noreply.github.com>
2024-10-24 11:37:43 +0200

    correction module 4 tp 1

--- app/src/main/java/com/example/eni_shop/MainActivity.kt
+++ app/src/main/java/com/example/eni_shop/MainActivity.kt
@@ -1,48 +1,15 @@
 package com.example.eni_shop
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
 import com.example.eni_shop.bo.Article
 import com.example.eni_shop.repository.ArticleRepository
-import com.example.eni_shop.ui.screen.ArticleDetailScreen
-import com.example.eni_shop.ui.screen.FormScreen
+import com.example.eni_shop.ui.screen.ArticleFormScreen
 import com.example.eni_shop.ui.theme.ENIShopTheme
-import java.text.SimpleDateFormat
 import java.util.Date
-import java.util.Locale
 
 
 private const val TAG = "MainActivity"
@@ -54,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ENIShopTheme {
 
-                FormScreen()
+                ArticleFormScreen()
             }
         }
 
--- app/src/main/java/com/example/eni_shop/ui/common/Components.kt
+++ app/src/main/java/com/example/eni_shop/ui/common/Components.kt
@@ -1,26 +1,41 @@
 package com.example.eni_shop.ui.common
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,27 +43,105 @@ import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EniShopTopBar(modifier : Modifier = Modifier) {
-    TopAppBar(title = { EniShopTopBarTitle()})
+fun EniShopTopBar(modifier: Modifier = Modifier) {
+    TopAppBar(title = { EniShopTopBarTitle() })
 }
 
 @Composable
-fun EniShopTopBarTitle(modifier : Modifier = Modifier) {
+fun EniShopTopBarTitle(modifier: Modifier = Modifier) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center,
-        modifier = modifier.fillMaxWidth()){
-        Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = "Shop", modifier=Modifier.size(40.dp))
-        Spacer(modifier= Modifier.width(8.dp))
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Icon(
+            imageVector = Icons.Default.ShoppingCart,
+            contentDescription = "Shop",
+            modifier = Modifier.size(40.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
         Text(
             text = "ENI-SHOP",
             color = MaterialTheme.colorScheme.tertiary,
-            fontSize = 40.sp)
+            fontSize = 40.sp
+        )
+    }
+}
+
+@Composable
+fun EniShopTextField(
+    modifier: Modifier = Modifier,
+    label: String,
+    value: String = "",
+    onValueChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    enabled: Boolean = true,
+    placeholder: (@Composable () -> Unit) = {},
+    trailingIcon: @Composable (() -> Unit)? = {}
+) {
+    Surface(
+        border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary),
+        shape = RoundedCornerShape(10),
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Column(modifier = Modifier.padding(all = 8.dp)) {
+            Text(
+                text = label, style = MaterialTheme.typography.labelLarge,
+                fontSize = 24.sp
+            )
+            TextField(
+                value = value,
+                onValueChange = onValueChange,
+                modifier = Modifier.fillMaxWidth(),
+                enabled = enabled,
+                keyboardOptions = keyboardOptions,
+                placeholder = placeholder,
+                trailingIcon = trailingIcon
+            )
+        }
+    }
+}
+
+@Composable
+fun DropDownMenuCategories() {
+    val categories = mutableListOf("electronics", "jewelery", "men's clothing", "women's clothing")
+    var expanded by remember { mutableStateOf(false) }
+    var selectedCategory by remember { mutableStateOf("") }
+
+    Column {
+        EniShopTextField(
+            modifier = Modifier.clickable {
+                expanded = !expanded
+            },
+            label = "Catégorie", value = selectedCategory, onValueChange = {}, enabled = false,
+            placeholder = { Text("Choisir une catégorie")},
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = "Dropdown"
+                )
+            }
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            categories.forEach() {
+                DropdownMenuItem(
+                    text = { Text(text = it) },
+                    onClick = {
+                        selectedCategory = it
+                        expanded = false
+                    }
+                )
+            }
+        }
     }
 }
 
 @Preview
 @Composable
 fun Preview() {
-    EniShopTopBar()
+    EniShopTextField(label = "nom", onValueChange = {})
 }
